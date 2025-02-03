6c650a12c5b6959dc73052c4193b4448c3c04428
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-03 17:18:34 +0530

    #

--- angular_learn/src/app/app.component.ts
+++ angular_learn/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  isLoggedIn : boolean = true;
+  
+  names : string[] = ['madara','withana','sandeepani','rusiru','chapana'];
 
 }

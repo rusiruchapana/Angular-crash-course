07d942dbd36cb42092d22fed01c733f42112f043
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-25 21:32:21 +0530

    component relationshipd

--- angular_learn/src/app/app.component.ts
+++ angular_learn/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FetchListComponent } from "./components/fetch-list/fetch-list.component";
+import { ParentComponent } from "./components/parent/parent.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FetchListComponent],
+  imports: [FetchListComponent, ParentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
